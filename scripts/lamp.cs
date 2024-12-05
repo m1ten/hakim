b8c1f9a26380743843936af65868b632b575af88
fourarmy <57693631+m1ten@users.noreply.github.com>
2024-12-05 14:57:29 -0500

    optimize

--- scripts/lamp.cs
+++ scripts/lamp.cs
@@ -4,70 +4,79 @@ namespace hakim.scripts;
 
 public partial class Lamp : Sprite2D
 {
-	private PointLight2D _light;
-	private bool _isOn = true;
-	private bool _isTransitioning;
-	private const float TransitionSpeed = 4.0f;
-	private float _currentEnergy;
-	private float _targetEnergy;
-	private float _currentAlpha;
-	private float _targetAlpha;
+    private const float TransitionSpeed = 4.0f;
+    private const float LightOnEnergy = 2.0f;
+    private const float LightOffEnergy = 0.3f;
+    private const float LightOnAlpha = 1.0f;
+    private const float LightOffAlpha = 0.1f;
+    private const float LightOnScale = 1.5f;
+    private const float LightOffScale = 1.0f;
+    private const float ModulateIntensity = 1.5f;
 
-	public override void _Ready()
-	{
-		_light = GetNode<PointLight2D>("PointLight2D");
-		_currentEnergy = 2.0f;
-		_targetEnergy = 2.0f;
-		_currentAlpha = 1.0f;
-		_targetAlpha = 1.0f;
-	}
+    private PointLight2D _light;
+    private bool _isOn = true;
+    private bool _isTransitioning;
+    private float _currentEnergy;
+    private float _targetEnergy;
+    private float _currentAlpha;
+    private float _targetAlpha;
+    private float _currentScale;
+    private float _targetScale;
 
-	public override void _Input(InputEvent @event)
-	{
-		if (@event is InputEventMouseButton mouseButton)
-		{
-			if (mouseButton.ButtonIndex == MouseButton.Left && mouseButton.Pressed)
-			{
-				var clickPosition = mouseButton.Position;
-				if (GetRect().HasPoint(ToLocal(clickPosition)))
-				{
-					ToggleLight();
-				}
-			}
-		}
-	}
+    public override void _Ready()
+    {
+        _light = GetNode<PointLight2D>("PointLight2D");
+        _currentEnergy = LightOnEnergy;
+        _targetEnergy = LightOnEnergy;
+        _currentAlpha = LightOnAlpha;
+        _targetAlpha = LightOnAlpha;
+        _currentScale = LightOnScale;
+        _targetScale = LightOnScale;
+    }
 
-	private void ToggleLight()
-	{
-		_isTransitioning = true;
-		if (_isOn)
-		{
-			_isOn = false;
-			_targetEnergy = 0.3f;
-			_targetAlpha = 0.1f;
-		}
-		else
-		{
-			_isOn = true;
-			_targetEnergy = 2.0f;
-			_targetAlpha = 1.0f;
-		}
-	}
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is not InputEventMouseButton mouseButton || 
+            mouseButton.ButtonIndex != MouseButton.Left || 
+            !mouseButton.Pressed) return;
 
-	public override void _Process(double delta)
-	{
-		if (_isTransitioning)
-		{
-			_currentEnergy = Mathf.MoveToward(_currentEnergy, _targetEnergy, TransitionSpeed * (float)delta);
-			_currentAlpha = Mathf.MoveToward(_currentAlpha, _targetAlpha, TransitionSpeed * (float)delta);
+        if (GetRect().HasPoint(ToLocal(mouseButton.Position)))
+        {
+            ToggleLight();
+        }
+    }
 
-			Modulate = new Color(1.5f, 1.5f, 1.5f, _currentAlpha);
-			_light.SetEnergy(_currentEnergy);
+    private void ToggleLight()
+    {
+        _isTransitioning = true;
+        _isOn = !_isOn;
+        
+        (_targetEnergy, _targetAlpha, _targetScale) = _isOn 
+            ? (LightOnEnergy, LightOnAlpha, LightOnScale)
+            : (LightOffEnergy, LightOffAlpha, LightOffScale);
+    }
 
-			if (Mathf.IsEqualApprox(_currentEnergy, _targetEnergy) && Mathf.IsEqualApprox(_currentAlpha, _targetAlpha))
-			{
-				_isTransitioning = false;
-			}
-		}
-	}
+    public override void _Process(double delta)
+    {
+        if (!_isTransitioning) return;
+
+        var deltaSpeed = TransitionSpeed * (float)delta;
+        _currentEnergy = Mathf.MoveToward(_currentEnergy, _targetEnergy, deltaSpeed);
+        _currentAlpha = Mathf.MoveToward(_currentAlpha, _targetAlpha, deltaSpeed);
+        _currentScale = Mathf.MoveToward(_currentScale, _targetScale, deltaSpeed);
+
+        Modulate = new Color(ModulateIntensity, ModulateIntensity, ModulateIntensity, _currentAlpha);
+        _light.SetEnergy(_currentEnergy);
+        _light.TextureScale = _currentScale;
+
+        if (IsTransitionComplete())
+        {
+            _isTransitioning = false;
+        }
+    }
+
+    private bool IsTransitionComplete() => 
+        Mathf.IsEqualApprox(_currentEnergy, _targetEnergy) &&
+        Mathf.IsEqualApprox(_currentAlpha, _targetAlpha) &&
+        Mathf.IsEqualApprox(_currentScale, _targetScale);
 }
